2594a62988d75734e6ff11a869de85529c08753e
Mitansh Patel <mitanshpatel@Mitanshs-Air.lan>
2025-07-13 01:11:59 -0700

    Inngest Fixed

--- ai-ticket-sorter/inngest/client.js
+++ ai-ticket-sorter/inngest/client.js
@@ -2,5 +2,5 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   name: "ModMatch",
-  eventKey: process.env.INNGEST_EVENT_KEY, // ✅ must be here
+  eventKey: process.env.INNGEST_EVENT_KEY, // ✅ this is now your signing key reused
 });
